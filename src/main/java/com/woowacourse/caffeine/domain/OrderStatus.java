fc292db4627c9801308969fe44afca129ce6bf18
MK S <conatuseus@gmail.com>
2019-12-03 19:07:14 +0900

    feat: add OrderStatus for checking order Status

---
+++ src/main/java/com/woowacourse/caffeine/domain/OrderStatus.java
@@ -0,0 +1,20 @@
+package com.woowacourse.caffeine.domain;
+
+public enum  OrderStatus {
+    CANCEL("주문 취소"),
+    SUCCESS("주문 성공"),
+    FAIL("주문 실패"),
+    IN_PROGRESS("준비 중"),
+    READY("준비 완료"),
+    FINISH("수령 완료");
+
+    private String status;
+
+    OrderStatus(final String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
