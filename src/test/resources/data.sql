9bf352e953d707bfc6531f38b7ac370264734402
MK S <conatuseus@gmail.com>
2019-12-10 15:20:19 +0900

    refactor: MenuItem 삭제 테스트를 위한 데이터 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO shop(id, name) VALUES(100, '어디야 커피 잠실점');
 INSERT INTO menu_item(id ,name, description, price, shop_id) VALUES(987654321L,'카페라떼', '고소한 카페라떼', 3000, 100);
 INSERT INTO menu_item(id, name, description, price, shop_id) VALUES(987654322L, '고구마라떼', '달달한 라떼', 3000, 100);
+INSERT INTO menu_item(id, name, description, price, shop_id) VALUES(987654323L, '얼음물', '시원한 물', 100000, 100);
