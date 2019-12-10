3c918fa5a95b539d4daa417934cc91a946df04d4
MK S <conatuseus@gmail.com>
2019-12-10 16:32:48 +0900

    feat: 코드 포맷팅 및 MenuItem 엔티티, 서비스 테스트

--- src/test/java/com/woowacourse/caffeine/application/service/MenuItemInternalServiceTest.java
+++ src/test/java/com/woowacourse/caffeine/application/service/MenuItemInternalServiceTest.java
@@ -1,9 +1,11 @@
 package com.woowacourse.caffeine.application.service;
 
 import com.woowacourse.caffeine.application.dto.MenuCreateRequest;
+import com.woowacourse.caffeine.application.dto.MenuItemUpdateRequest;
 import com.woowacourse.caffeine.domain.MenuItem;
 import com.woowacourse.caffeine.domain.Shop;
 import com.woowacourse.caffeine.repository.MenuItemRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,8 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static java.util.Optional.ofNullable;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,10 +33,26 @@ class MenuItemInternalServiceTest {
     @InjectMocks
     private MenuItemInternalService menuItemInternalService;
 
+    private Shop shop;
+    private MenuItem menuItem;
+
+    @BeforeEach
+    void setUp() {
+        shop = new Shop("어디야 커피");
+        menuItem = MenuItem.builder()
+            .name("아이스 아메리카노")
+            .nameInEnglish("Ice Americano")
+            .description("시원한 아메리카노")
+            .price(2000)
+            .img("abc")
+            .category("coffee")
+            .vendor(shop)
+            .build();
+    }
+
     @Test
     void findByShopId() {
         // given
-        Shop shop = new Shop("어디야 커피");
         MenuItem americano = MenuItem.builder()
             .name("아메리카노")
             .nameInEnglish("Americano")
@@ -65,36 +86,46 @@ class MenuItemInternalServiceTest {
     @Test
     void createMenuItem() {
         //given
-        Shop shop = new Shop("어디야 커피");
-        long shopId = 1;
-
-        MenuItem menuItem = MenuItem.builder()
-            .name("아이스 아메리카노")
-            .nameInEnglish("Ice Americano")
-            .description("시원한 아메리카노")
-            .price(2000)
-            .img("abc")
-            .category("coffee")
-            .vendor(shop)
-            .build();
-
+        long shopId = 1L;
         MenuCreateRequest menuCreateRequest = new MenuCreateRequest(
             "아이스 아메리카노", "Ice Americano", "시원한 아메리카노", 2000, "abc", "coffee", shopId);
 
         //when
-        when(menuItemInternalService.createMenuItem(menuCreateRequest)).thenReturn(menuItem);
+        when(menuItemRepository.save(any())).thenReturn(menuItem);
         MenuItem created = menuItemInternalService.createMenuItem(menuCreateRequest);
 
         //then
-        assertThat(menuItem).isNotNull();
-//        assertThat(created).isEqualTo(menuItem);
+        assertThat(menuItem).isEqualTo(created);
     }
 
     @Test
     void findByMenuItemId() {
+        // given
+        long menuId = 1L;
+
+        // when
+        when(menuItemRepository.findById(menuId)).thenReturn(ofNullable(menuItem));
+        MenuItem foundMenuItem = menuItemInternalService.findByMenuItemId(menuId);
+
+        // then
+        assertThat(foundMenuItem).isEqualTo(menuItem);
     }
 
     @Test
     void updateMenuItem() {
+        // given
+        long menuId = 1L;
+        MenuItemUpdateRequest menuItemUpdateRequest = new MenuItemUpdateRequest(
+            "뜨거운 아메리카노", "Hot Americano", "So Hot Ame", 2000, "abcd", "coffee");
+
+        // when
+        when(menuItemRepository.findById(menuId)).thenReturn(Optional.of(menuItem));
+
+        // then
+        MenuItem updatedMenuItem = menuItemInternalService.updateMenuItem(menuId, menuItemUpdateRequest);
+        assertThat(updatedMenuItem.getName()).isEqualTo("뜨거운 아메리카노");
+        assertThat(updatedMenuItem.getNameInEnglish()).isEqualTo("Hot Americano");
+        assertThat(updatedMenuItem.getPrice()).isEqualTo(2000);
+
     }
 }
